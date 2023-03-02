f00e477366585c7f9d66ac314183ad3dd34be4b6
Ty Aponte <aaustinsk8@aol.com>
2023-03-02 14:29:38 -0800

    initial

---
+++ client/cashier/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const {io} = require('socket.io-client');
+const socket = io.connect('http://localhost:3001/sand');
+
+var Chance = require('chance');
+var chance = new Chance();
+
+const generateSandwhich=()=>{
+  let payload = {
+    orderId: chance.guid(),
+    customer: chance.name(),
+  }
+  socket.emit('ORDER-READY', payload);
+}
+
+socket.on('ORDER-COMPLETE', (payload)=>{
+  socket.emit('ORDER-DELIVERED', payload);
+});
+
+setInterval(()=>{
+  generateSandwhich();
+},2000);
+
+// socket.emit('ORDER READY', payload);
---
+++ client/cook/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const {io} = require('socket.io-client');
+const socket = io.connect('http://localhost:3001/sand');
+
+socket.on('ORDER-READY', (payload)=>{
+  setTimeout(()=>{
+    socket.emit('ORDER-COMPLETE', payload);
+  },1000);
+});
+
