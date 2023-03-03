bc5f66624b142126b9cdfd68a30153a5985d92b7
MARTIN HANSEN <marrtt1234@gmail.com>
2023-03-03 08:42:39 +0000

    queue implemented

--- client/cashier/index.js
+++ client/cashier/index.js
@@ -6,20 +6,29 @@ const socket = io.connect('http://localhost:3001/sand');
 var Chance = require('chance');
 var chance = new Chance();
 
+socket.emit('GET-ORDERS', {queueId: 'sandwich-ORDER-COMPLETE'});
+
 const generateSandwich=()=>{
   let payload = {
     orderId: chance.guid(),
     customer: chance.name(),
+    queueId: 'sandwich',
+    currentEmit: '',
   };
+
+  payload.currentEmit = 'ORDER-READY';
+  console.log('[cashier/index.js 20] PAYLOAD.CURRENT-EMIT:',payload.currentEmit);
   socket.emit('ORDER-READY', payload);
 };
 
 socket.on('ORDER-COMPLETE', (payload)=>{
+  payload.currentEmit = 'ORDER-DELIVERED';
+  console.log('[cashier/index.js 26] PAYLOAD.CURRENT-EMIT:',payload.currentEmit);
   socket.emit('ORDER-DELIVERED', payload);
 });
 
 setInterval(()=>{
   generateSandwich();
-},2000);
+},6000);
 
 // socket.emit('ORDER READY', payload);
